f463fa6aa27ce2053643feedb2eca85578ef4c4e
Anshuman Suri <iamgroot42@users.noreply.github.com>
2024-05-10 14:08:24 -0400

    Update table3.sh

--- scripts/paper_scripts/table3.sh
+++ scripts/paper_scripts/table3.sh
@@ -5,7 +5,7 @@ for ngram in "7" "13"
 do
     for subset in "wikipedia_(en)" "github" "pubmed_central" "pile_cc" "arxiv"
     do
-        knocky python run.py \
+        python run.py \
             --config configs/mi.json \
             --revision step99000 \
             --base_model "EleutherAI/pythia-12b-deduped" \
