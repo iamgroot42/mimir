51fd3c3e91a6edb2274da052504a891eb92c3cfc
Anshuman Suri <iamgroot42@users.noreply.github.com>
2024-05-10 14:08:12 -0400

    Update table11.sh

--- scripts/paper_scripts/table11.sh
+++ scripts/paper_scripts/table11.sh
@@ -6,7 +6,7 @@ for model in "gpt-neo-125m" "gpt-neo-1.3B" "gpt-neo-2.7B"
 do
     for subset in "wikipedia_(en)" "github" "pile_cc" "pubmed_central" "arxiv" "dm_mathematics" "hackernews"
     do
-        knocky python run.py \
+        python run.py \
             --config configs/mi.json \
             --base_model "EleutherAI/${model}" \
             --specific_source ${subset}_ngram_${ngram}_\<0.8_truncated \
